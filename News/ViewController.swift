415a26d8f136b19392ef6b28ff5065cc98f4e7a8
fixik <irinagomel10@gmail.com>
2025-04-28 20:34:50 +0300

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Эдуард Кудянов on 28.04.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
